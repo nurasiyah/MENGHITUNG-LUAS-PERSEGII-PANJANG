0e0a3e2e4e6b550a718697769996ee1a612b7639
nurasiyah <45659380+nurasiyah@users.noreply.github.com>
2019-01-05 14:54:32 +0700

    Add files via upload

---
+++ menghitung luas perseggi panjang.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int p,l,luas;
+    cout<<"Masukkan panjang = ";
+    cin>>p;
+    cout<<"Masukkan lebar = ";
+    cin>>l;
+
+    luas = p*l;
+    cout<<"Luas persegi panjang adalah = "<<luas;
+    return 0;
+}
